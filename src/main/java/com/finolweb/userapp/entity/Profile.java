d417710be36a63fe214b033f01dd18849e11c808
Jose Alexander Finol <jfinol@analytics.pe>
2021-09-22 22:19:17 -0400

    'changes'

---
+++ src/main/java/com/finolweb/userapp/entity/Profile.java
@@ -0,0 +1,92 @@
+package com.finolweb.userapp.entity;
+
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "profile")
+public class Profile {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "lastname")
+	private String lastName;
+
+	@Column(name = "birthdate")
+	//@Temporal(TemporalType.DATE)
+	private Date birthDate;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Profile(Integer id, String name, String lastName, Date birthDate) {;
+		this.id = id;
+		this.name = name;
+		this.lastName = lastName;
+		this.birthDate = birthDate;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Profile other = (Profile) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+}
+
